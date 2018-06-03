a071779e51bff5918c66217eeeb12be62e0e80c8
JuanMa <juanma.garrido@gmail.com>
2018-06-03 13:18:40 +0200

    changed Name HOC

--- src/scenario4/hoc/withDidMountStateMessages.js
+++ src/scenario4/hoc/withDidMountStateMessages.js
@@ -1,23 +1,19 @@
 import { lifecycle } from "recompose";
 
 const getPendingMessages = () => {
-  const randomNumber = Math.ceil(Math.random()*10)
-  return new Promise( resolve => {
-    setTimeout(
-      () => resolve(randomNumber),
-      randomNumber * 1000
-    )
-  })
-}
+  const randomNumber = Math.ceil(Math.random() * 10);
+  return new Promise(resolve => {
+    setTimeout(() => resolve(randomNumber), randomNumber * 1000);
+  });
+};
 
-const withPendingNotificacions = lifecycle({
+const withDidMountStateMessages = lifecycle({
   componentDidMount() {
-    this.setState({ loading: true })
-    getPendingMessages()
-      .then(messages => {
-        this.setState({ loading: false, messages });
-      })
+    this.setState({ loading: true });
+    getPendingMessages().then(messages => {
+      this.setState({ loading: false, messages });
+    });
   }
-})
+});
 
-export default withPendingNotificacions;
+export default withDidMountStateMessages;
